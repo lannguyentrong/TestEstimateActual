29dceffdd75e3ebfc5802415d621f4f2bd283d5b
lannguyentrong <lannguyentrong001@gmail.com>
2022-01-10 19:57:56 +0700

    Project Initialization Done

---
+++ TestEstimateActual/TestEstimateActual/Models/DatabaseContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestEstimateActual.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        { }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Actual> Actuals { get; set; }
+        public DbSet<Estimate> Estimates { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.GetConnectionString("TestEstimateActualConnectionString");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Actual>()
+                .Property(item => item.State)
+                .ValueGeneratedNever();
+
+            // Set PK For "Estimate"
+            modelBuilder.Entity<Estimate>().HasKey(item => new { item.State, item.District }).HasName("estimate_pk");
+        }
+    }
+}
---
+++ TestEstimateActual/TestEstimateActual/Models/Estimate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestEstimateActual.Models
+{
+    public class Estimate
+    {
+        public int State { get; set; }
+        public int District { get; set; }
+        public int EstimatePopulation { get; set; }
+        public int EstimateHouseholds { get; set; }
+    }
+}
